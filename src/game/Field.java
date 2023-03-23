bec37065015e7961604764f675dcb1c47f31a5c5
slava <fclmc@mail.ru>
2023-03-23 14:36:32 +0300

    Добавил инициализацию поля

--- src/game/Field.java
+++ src/game/Field.java
@@ -7,20 +7,38 @@ public class Field {
     public Field(int x, int y) {
         this.field = new Cell[x][y];
 
-        //инициализация
-        /*for (;;) {
-            for (;;) {
-
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                field[i][j] = new Cell();
             }
         }
 
 
         //установить соседа у каждой ячейки вызвать  setNeighbour
-        for (;;) {
-            for (;;) {
-
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (i == 0) {
+                    if (j == 0) {
+                        field[i][j].setNeighbour(null, field[1][0], field[0][1], null); }
+                    else if (j < field[i].length - 1) {
+                        field[i][j].setNeighbour(null, field[1][j], field[1][j+1], field[1][j-1] ); }
+                    else if (j == field[i].length - 1) {
+                        field[i][j].setNeighbour(null, field[1][j], null, field[0][j-1] ); }
+                } else if (i == field.length-1) {
+                    if (j == 0) {
+                        field[i][j].setNeighbour(field[i-1][j], null, field[i][1], null);}
+                    else if (j < field[i].length) {
+                        field[i][j].setNeighbour(field[i-1][j], null, field[i][j+1], field[i][j-1]);}
+                    else if (j == field[i].length) {
+                        field[i][j].setNeighbour(field[i-1][j], null, null, field[i][j-1]);}
+                } else if (y == 0) {
+                    field[i][j].setNeighbour(field[i-1][j], field[i+1][j], field[i][j+1], null); }
+                else if (y == field[i].length-1) {
+                    field[i][j].setNeighbour(field[i-1][j], field[i+1][j], null, field[i][j-1]);}
+                else {
+                    field[i][j].setNeighbour(field[i-1][j], field[i+1][j], field[i][j+1], field[i][j-1]);}
             }
-        }*/
+        }
     }
 
     /**
